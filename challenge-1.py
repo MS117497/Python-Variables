2a4a60722d898234b22973facdcea864ddeb29a5
Michael Spagnola <Michael@administrators-iMac.local>
2018-02-06 20:23:46 -0700

    Initial commit of challenge 1.

---
+++ challenge-1.py
@@ -0,0 +1,7 @@
+fav_search_engine = str('Google')
+year = int(2015)
+next_olympics = int(2016)
+
+print 'fav_search_engine: ' + fav_search_engine
+print 'year: ' + str(year)
+print 'next olympics: ' + str(next_olympics)
