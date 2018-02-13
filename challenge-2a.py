3092a6c6ec1671435ed40fbc0d21a131a1d37a74
Michael Spagnola <Michael@administrators-iMac.local>
2018-02-12 19:43:55 -0700

    Commit code corrections.

---
+++ challenge-2a.py
@@ -0,0 +1,8 @@
+magic_num = 11
+if magic_num > 6:
+    if magic_num > 10:
+        print "Biiiiiig number"
+    else:
+        print "Some number?"
+else:
+    print "Smaller number"
---
+++ challenge-3.py
@@ -0,0 +1,9 @@
+# Prints "The number is greater than 10"
+num = 11
+if num > 6:
+    if num > 10:
+        print "The number is greater than 10"
+    else:
+        print "The number is greater than 6 but not greater than 10"
+else:
+    print "The number is less than or equal to 6."
